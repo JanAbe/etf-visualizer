ff3153ecfe4271e2a63596b0dcaa97fbacdeeead
Jan Abe de Waal <jandewaal33@gmail.com>
2020-02-24 14:44:19 +0100

    Add react-mapbox-gl, create simple map
    Note: the mapbox token is written in the code itself, and is checked in git. Atm I don't care, this will be changed later

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactMapGL from 'react-map-gl';
 import './index.css';
 
-const element = <h1>Hello, world</h1>;
-ReactDOM.render(element, document.getElementById('root'));
+// todo: remove this, place in config file, or in env variable.
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoid2ludGVyLW1vb24iLCJhIjoiY2s2dXE1dHI1MGJsZDNma2hlbnI2Z3NvciJ9.3Fomq0bT2ITqqqvCCUi2dg';
+
+class Map extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                width: 400,
+                height: 400,
+                latitude: 37.7577,
+                longitude: -122.4376,
+                zoom: 8
+            }
+        };
+    }
+
+    render() {
+        return (
+            <ReactMapGL 
+                mapboxApiAccessToken={MAPBOX_TOKEN}
+                {...this.state.viewport}
+                onViewportChange={(viewport) => this.setState({viewport: viewport})}
+            />
+        );
+    }
+}
+
+ReactDOM.render(<Map />, document.getElementById('root'));
