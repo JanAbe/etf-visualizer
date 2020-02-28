81b47a5a1115584ea74c7e60d9c21632e142ac7e
Jan Abe de Waal <jandewaal33@gmail.com>
2020-02-28 17:52:38 +0100

    Add deckGL map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { StaticMap } from 'react-map-gl';
+import DeckGL, { LineLayer } from 'deck.gl';
 
 // todo: remove this, place in config file, or in env variable.
 const MAPBOX_TOKEN = 'pk.eyJ1Ijoid2ludGVyLW1vb24iLCJhIjoiY2s2dXE1dHI1MGJsZDNma2hlbnI2Z3NvciJ9.3Fomq0bT2ITqqqvCCUi2dg';
+
+const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
+// Viewport settings
+const viewState = {
+    latitude: 38.022672,
+    longitude: 9.188889,
+    zoom: 1.52,
+    pitch: 0,
+    bearing: 0
+  };
+
 
 class Map extends React.Component {
     constructor(props) {
@@ -29,16 +41,42 @@ class Map extends React.Component {
         const expandedWidth = '60%';
         const transitionSettings = 'width 0.8s';
 
+        // return (
+        //     <ReactMapGL
+        //         style={{transition: transitionSettings}}
+        //         {...this.state.viewport}
+        //         height='100%'
+        //         width={expanded ? expandedWidth : defaultWidth}
+        //         mapStyle={prefersDarkMode ? darkMapURL : lightMapURL}
+        //         onViewportChange={this.onViewportChange}
+        //         mapboxApiAccessToken={MAPBOX_TOKEN}
+        //     />
+        // );
+
+        const layers = [
+            new LineLayer({id: 'line-layer', data})
+        ];
+
         return (
             <ReactMapGL
                 style={{transition: transitionSettings}}
-                {...this.state.viewport}
                 height='100%'
                 width={expanded ? expandedWidth : defaultWidth}
-                mapStyle={prefersDarkMode ? darkMapURL : lightMapURL}
-                onViewportChange={this.onViewportChange}
-                mapboxApiAccessToken={MAPBOX_TOKEN}
-            />
+                disableTokenWarning={true}
+            >
+                <DeckGL  // todo: find out how to get light and darkthemes of the deckGL/staticMap 
+                    initialViewState={viewState} 
+                    controller={true}
+                    layers={layers} 
+                    
+                >
+                    {prefersDarkMode ? (
+                        <StaticMap mapStyle={darkMapURL} mapboxApiAccessToken={MAPBOX_TOKEN} />
+                    ) : (
+                        <StaticMap mapboxApiAccessToken={MAPBOX_TOKEN} />
+                    )}
+                </DeckGL>
+            </ReactMapGL>
         );
     }
 }
